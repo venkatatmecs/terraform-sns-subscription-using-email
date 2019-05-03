373306046173e529333256b3d86f4764f070eed6
muffy <muffy@instacart.com>
2019-05-02 17:04:50 -0700

    Run terraform fmt

--- examples/example1.tf
+++ examples/example1.tf
@@ -5,6 +5,6 @@ module "example1" {
   # source = "github.com/instacart/terraform-sns-subscription-using-email?ref=v1.0.0"
   source = ".."
 
-  name = "mytopic"
+  name  = "mytopic"
   email = "mytopic@example.com"
 }
